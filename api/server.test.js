56ca212123ecf79774c07e4a8f90cc72de543c9c
JonathanMary <jon85ma@gmail.com>
2021-03-26 17:25:30 +0100

    MVP: setting up tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,34 @@
-// Write your tests here
+const server = require('./server');
+const request = require('supertest');
+const db = require('../data/dbConfig');
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+beforeEach(async () => {
+  await db('users').truncate();
+  //await db.seed.run();
+})
+afterAll(async () => {
+  await db.destroy();
+})
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+})
+test('process.env.DB_ENV must be "testing"', () => {
+  expect(process.env.NODE_ENV).toBe('testing');
+})
+
+describe('API endpoints', () => {
+  describe('[POST] /api/auth/register', () => {
+    
+  })
+  describe('[POST] /api/auth/login', () => {
+    
+  })
+  describe('[GET] /api/jokes', () => {
+    
+  })
 })
